d8b86a3c062521d3dcd72f10cdc5c32aa36008a6
Maxime Dor <mdor@shi-r6-p2.s19.be>
2024-08-26 16:53:38 +0200

    creating models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
-# Create your models here.
+# Player class, once set is added to the DB
+
+class Player(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    scores = ArrayField(models.IntegerField(), default=list, blank=True)
+    tournaments = models.ManyToManyField('Tournament', related_name='players', blank=True)
+    def __str__(self):
+        return self.username
+
+# Tournament class, once set is added to the DB 
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
